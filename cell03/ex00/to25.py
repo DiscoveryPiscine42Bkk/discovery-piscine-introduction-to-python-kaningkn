2fcc1f3da31d0eeea86876e024a83f2a98d2a136
kaningkn <kaningkaning06@gmail.com>
2025-06-22 09:36:14 +0000

    .

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+try:
+    num = int(input("Enter a number less than 25:"))
+except ValueError:
+    print("Error")
+    exit()
+
+if num > 25:
+    print("Error")
+else:
+    while num < 25:
+        print(f"Inside the loop, my my variable is {num}")
+        num += 1
+
